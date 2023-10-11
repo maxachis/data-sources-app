8f1b223c87597b2882d64a8a482df25b71c09a8c
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-10-11 19:13:52 -0400

    Set up initial advanced search functionality

--- resources/AdvancedSearch.py
+++ resources/AdvancedSearch.py
@@ -20,22 +20,24 @@ class AdvancedSearch(Resource):
         params = request.args.to_dict()
 
         conditions = ""
-        search_string = ['name', 'description', 'supplying_entity', 'originating_entity']
-        agency_search_keys = ['agency_name', 'state', 'county', 'municipality', 'agency_type', 'jurisdiction_type']
+        search_string = ['name', 'description', 'supplying_entity', 'originating_entity', 'source_url']
+        agency_search_keys = ['agency_name', 'state_iso', 'county_name', 'municipality', 'agency_type', 'jurisdiction_type']
         for key, value in params.items():
             if key in search_string:
                 nlp = spacy.load('en_core_web_sm')
                 doc = nlp(value)
                 lemmatized_tokens = [token.lemma_ for token in doc]
                 new_value = " ".join(lemmatized_tokens)
-                conditions += f"data_sources.{key} ILIKE %{new_value}%"
+                conditions += f"data_sources.{key} ILIKE '%{new_value}%'"
             elif key in agency_search_keys:
                 if key == "agency_name":
-                  conditions += f"agencies.name = {value}"
+                    conditions += f"agencies.name = '{value}'"
+                elif key == "county_name":
+                    conditions += f"agencies.county_name = '[\"{value}\"]'"
                 else:
-                  conditions += f"agencies.{key} = {value}"
+                    conditions += f"agencies.{key} = '{value}'"
             else:
-                conditions += f"data_sources.{key} = {value}"
+                conditions += f"data_sources.{key} = '{value}'"
             conditions += " AND "
 
         if 'records_not_online' not in params:
@@ -65,8 +67,7 @@ class AdvancedSearch(Resource):
                 data_sources ON agency_source_link.airtable_uid = data_sources.airtable_uid
             INNER JOIN
                 agencies ON agency_source_link.agency_described_linked_uid = agencies.airtable_uid
-            WHERE
-                
+            WHERE 
         """
 
         sql_query_with_conditions = sql_query + conditions
@@ -74,35 +75,24 @@ class AdvancedSearch(Resource):
         cursor.execute(sql_query_with_conditions)
 
         results = cursor.fetchall()
-        print(results)
 
-        # column_names = ['data_source_name', 'description', 'record_type', 'source_url', 'record_format', 'coverage_start', 'coverage_end', 'agency_supplied', 'agency_name', 'municipality', 'state_iso']
+        column_names = ['data_source_name', 'description', 'record_type', 'source_url', 'record_format', 'coverage_start', 'coverage_end', 'agency_supplied', 'agency_name', 'municipality', 'state_iso']
 
-        # data_source_matches = [dict(zip(column_names, result)) for result in results]
+        data_source_matches = [dict(zip(column_names, result)) for result in results]
 
-        # for item in data_source_matches:
-        #    convert_dates_to_strings(item)
+        for item in data_source_matches:
+           convert_dates_to_strings(item)
 
-        # data_sources = {
-        #     "count": len(data_source_matches),
-        #     "data": data_source_matches
-        # }
-
-        # current_datetime = datetime.datetime.now()
-        # datetime_string = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
-
-        # query_results = json.dumps(data_sources['data'])
-
-        # cursor_query_log = self.psycopg2_connection.cursor()
-        # sql_query_log = "INSERT INTO quick_search_query_logs (search, location, results, result_count, datetime_of_request) VALUES (%s, %s, %s, %s, %s)"
-        # cursor_query_log.execute(sql_query_log, (search, location, query_results, data_sources['count'], datetime_string))
-        # self.psycopg2_connection.commit()
+        data_sources = {
+            "count": len(data_source_matches),
+            "data": data_source_matches
+        }
 
         return data_sources
         
     except Exception as e:
         print(str(e))
         # webhook_url = os.getenv('WEBHOOK_URL')
-        # message = {'content': 'Error during quick search operation: ' + str(e) + "\n" + f"Search term: {search}\n" + f'Location: {location}'}
+        # message = {'content': 'Error during advanced search operation: ' + str(e) + "\n" + f"Query Params: {params}\n"}
         # requests.post(webhook_url, data=json.dumps(message), headers={"Content-Type": "application/json"})
         return data_sources
